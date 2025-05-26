fbca9e24bb127f5315a21a044809e646c68308fe
rishab797 <rishabsood109@gmail.com>
2025-05-26 21:08:50 +0530

    backend.py

--- backend.py
+++ backend.py
@@ -6,6 +6,15 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
